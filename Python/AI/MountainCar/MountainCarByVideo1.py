7eb90c3bf481c430e9bf112b1d02c391c0e347a2
GonterTj <122777514+GonterTj@users.noreply.github.com>
2023-03-16 23:15:44 +0100

    Update MountainCarByVideo1.py

--- Python/AI/MountainCar/MountainCarByVideo1.py
+++ Python/AI/MountainCar/MountainCarByVideo1.py
@@ -5,12 +5,20 @@ import gym
 env = gym.make("MountainCar-v0")
 env.reset()
 
+print(env.observation_space.high)
+print(env.observation_space.low)
+print(env.action_space.n)
+
+'''
+
 done = False
 
 while not done:
     action = 2
-    new_state, reward, done, _ = env.step(action)
-    print(new_state)
+    n_state, reward, done, state, info = env.step(action)
+    print(n_state)
     env.render()
 
 env.close()
+
+'''
