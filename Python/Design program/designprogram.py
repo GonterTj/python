310f7b6a89598d351a683a29abfda75521a8e471
GonterTj <122777514+GonterTj@users.noreply.github.com>
2023-03-16 22:55:30 +0100

    1
    art

---
+++ Python/Design program/designprogram.py
@@ -0,0 +1,20 @@
+from turtle import *
+def r(x,y):
+    rt(x)
+    fd(y)
+
+tracer(2)
+fd(100)
+bgcolor('black')
+color('cyan')
+width(3)
+
+for i in range(2003):
+    fd(i)
+    r(90,i)
+    r(90,i)
+    r(270,i)
+    r(90,i)
+    circle(100,90)
+
+done()
