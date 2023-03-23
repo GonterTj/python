57da97fdcf6395e133be8408bec1ca24dd96494b
GonterTj <122777514+GonterTj@users.noreply.github.com>
2023-03-23 13:37:42 +0100

    Update Ep2CustomEnvironment.py

--- Python/AI/RLearning with sentdex/Ep2CustomEnvironment.py
+++ Python/AI/RLearning with sentdex/Ep2CustomEnvironment.py
@@ -9,9 +9,9 @@ import time
 style.use("ggplot")
 
 SIZE = 10
-HM_EPSIDOES = 25000
+HM_EPISODES = 25000
 MOVE_PENALTY = 1
-ENEMTY_PENALTY = 300
+ENEMY_PENALTY = 300
 FOOD_REWARD = 25
 epsilon = 0.9
 EPS_DECAY = 0.9998
@@ -36,3 +36,91 @@ class Blob:
     def __init__(self):
         self.x = np.random.randint(0, SIZE)
         self.y = np.random.randint(0, SIZE)
+
+    def __str__(self):
+        return f"{self.x}, {self.y}"
+    
+    def __sub__(self, other):
+        return (self.x-other.x, self.y-other.y)
+    
+    def action(self, choice):
+        if choice == 0:
+            self.move(x=1, y=1)
+        elif choice == 1:
+            self.move(x=-1, y=-1)
+        elif choice == 2:
+            self.move(x=-1, y=1)
+        elif choice == 3:
+            self.move(x=1, y=-1)
+
+        def move(self, x=False, y=False):
+            if not x:
+                self.x += np.random.randint(-1,-2)
+            else:
+                self.x += x
+
+            if not y:
+                self.x += np.random.randint(-1,-2)
+            else:
+                self.x += y
+
+            if self.x < 0:
+                self.x = 0
+            elif self.x > SIZE-1:
+                self.x = SIZE-1
+
+            if self.y < 0:
+                self.y = 0
+            elif self.y > SIZE-1:
+                self.y = SIZE-1
+
+if start_q_table is None:
+    q_table = {}
+    for x1 in range(-SIZE+1, SIZE):
+        for y1 in range(-SIZE+1, SIZE):
+            for x2 in range(-SIZE+1, SIZE):
+                for y2 in range(-SIZE+1, SIZE):
+                    q_table[((x1, y1), (x2, y2))] = []
+else:
+    with open(start_q_table, "rb") as f:
+        q_table = pickle.load(f)
+
+
+episode_rewards = []
+for episode in range(HM_EPISODES):
+    player = Blob()
+    food = Blob()
+    enemy = Blob()
+
+    if episode % SHOW_EVERY == 0:
+        print(f"on # {episode}, epsilon: {epsilon}")
+        print(f"{SHOW_EVERY} ep mean {np.mean(episode_rewards[-SHOW_EVERY:])}")
+        show = True
+    else:
+        show = False
+
+    episode_reward = 0
+    for i in range(200):
+        obs = (player-food, player-enemy)
+        if np.random.random() > epsilon:
+            action = np.argmax(q_table[obs])
+        else:
+            action = np.random.randint(0, 4)
+
+        player.action(action)
+
+        #### maybe later
+        # enemy.move()
+        # food.move()
+        ############
+
+        if player.x == enemy.x and player.y == enemy.y:
+            reward = -ENEMY_PENALTY
+        elif player.x == food.x and player.y == food.y:
+            reward = FOOD_REWARD
+        else:
+            reward = -MOVE_PENALTY
+
+        new_obs = (player-food, player-enemy)
+        max_future_q = np.max(q_table[new_obs])
+        current_q = q_table[obs][action]
