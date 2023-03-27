a904f929af6712cc820c7a8e8fe04be387e91ddd
GonterTj <122777514+GonterTj@users.noreply.github.com>
2023-03-27 12:16:52 +0200

    Update Ep2CustomEnvironment.py

--- Python/AI/RLearning with sentdex/Ep2CustomEnvironment.py
+++ Python/AI/RLearning with sentdex/Ep2CustomEnvironment.py
@@ -53,26 +53,26 @@ class Blob:
         elif choice == 3:
             self.move(x=1, y=-1)
 
-        def move(self, x=False, y=False):
-            if not x:
-                self.x += np.random.randint(-1,-2)
-            else:
-                self.x += x
+    def move(self, x=False, y=False):
+        if not x:
+            self.x += np.random.randint(-1,-2)
+        else:
+            self.x += x
 
-            if not y:
-                self.x += np.random.randint(-1,-2)
-            else:
-                self.x += y
+        if not y:
+            self.x += np.random.randint(-1,-2)
+        else:
+            self.x += y
 
-            if self.x < 0:
-                self.x = 0
-            elif self.x > SIZE-1:
-                self.x = SIZE-1
+        if self.x < 0:
+            self.x = 0
+        elif self.x > SIZE-1:
+            self.x = SIZE-1
 
-            if self.y < 0:
-                self.y = 0
-            elif self.y > SIZE-1:
-                self.y = SIZE-1
+        if self.y < 0:
+            self.y = 0
+        elif self.y > SIZE-1:
+            self.y = SIZE-1
 
 if start_q_table is None:
     q_table = {}
@@ -124,3 +124,45 @@ for episode in range(HM_EPISODES):
         new_obs = (player-food, player-enemy)
         max_future_q = np.max(q_table[new_obs])
         current_q = q_table[obs][action]
+
+        if reward == FOOD_REWARD:
+            new_q = FOOD_REWARD
+        elif reward == -ENEMY_PENALTY:
+            new_q = -ENEMY_PENALTY
+        else:
+            new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
+
+        q_table[obs][action] = new_q
+
+        if show:
+            env = np.zeros((SIZE, SIZE, 3), dtype = np.uint8)
+            env[food.y][food.x] = d[FOOD_N]
+            env[player.y][player.x] = d[PLAYER_N]
+            env[enemy.y][enemy.x] = d[ENEMY_N]
+
+        img = Image.fromarray(env, "RGB")
+        img = img.resize((300,300))
+        cv2.imshow("uhm", np.array(img))
+        if reward == FOOD_REWARD or reward == -ENEMY_PENALTY:
+            if cv2.waitkey(500) & 0xFF == ord("q"):
+                break
+        else:
+            if cv2.waitkey(1) & 0xFF == ord("q"):
+                break
+
+        episode_reward += reward
+        if reward == FOOD_REWARD or reward == -ENEMY_PENALTY:
+            break
+
+    episode_reward.append(episode_reward)
+    epsilon *= EPS_DECAY
+
+moving_avg = np.convolve(episode_rewards, np.ones((SHOW_EVERY,)) / SHOW_EVERY, mode="valid")
+
+plt.plot([i for i in range(len(moving_avg))], moving_avg)
+plt.ylabel(f"reward {SHOW_EVERY}ma")
+plt.xlabel("episode #")
+plt.show()
+
+with open(f"qtable-{int(time.time())}.pickle", "wb") as f:
+    pickle.dump(q_table, f)
